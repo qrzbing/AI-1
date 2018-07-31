48f98e58f22785e64956a83ee76a7e5e8fd0134f
qrzbing <ikav@qq.com>
2018-07-31 14:43:25 +0800

    format files

--- cifar10_train.py
+++ cifar10_train.py
@@ -45,99 +45,99 @@ import tensorflow as tf
 import cifar10
 
 
-#FLAGS用于传递 tf.app.run( ) 所需的参数
+# FLAGS用于传递 tf.app.run( ) 所需的参数
 FLAGS = tf.app.flags.FLAGS
-#定义训练集路径
+# 定义训练集路径
 tf.app.flags.DEFINE_string('train_dir', '/tmp/cifar10_train',
                            """Directory where to write event logs """
                            """and checkpoint.""")
-#训练轮数
+# 训练轮数
 tf.app.flags.DEFINE_integer('max_steps', 1000000,
                             """Number of batches to run.""")
-#是否打印设备分配日志，找指令和张量被分配到哪个设备
+# 是否打印设备分配日志，找指令和张量被分配到哪个设备
 tf.app.flags.DEFINE_boolean('log_device_placement', False,
                             """Whether to log device placement.""")
-#打印日志频率
+# 打印日志频率
 tf.app.flags.DEFINE_integer('log_frequency', 10,
                             """How often to log results to the console.""")
 
 
 def train():
-  """Train CIFAR-10 for a number of steps."""
-  with tf.Graph().as_default():
-      #as_default()用于保存变量信息
+    """Train CIFAR-10 for a number of steps."""
+    with tf.Graph().as_default():
+        # as_default()用于保存变量信息
 
-    #记录全局更新轮数
-    global_step = tf.contrib.framework.get_or_create_global_step()
+        # 记录全局更新轮数
+        global_step = tf.contrib.framework.get_or_create_global_step()
 
-    # Get images and labels for CIFAR-10.
-    #获取数据集和标签
-    images, labels = cifar10.distorted_inputs()
+        # Get images and labels for CIFAR-10.
+        # 获取数据集和标签
+        images, labels = cifar10.distorted_inputs()
 
-    # Build a Graph that computes the logits predictions from the
-    # inference model.
-    #构建一个图形，用于计算推理模型中的logits的预测值
-    #使用CNN，经过卷积和池化层处理。
-    logits = cifar10.inference(images)
+        # Build a Graph that computes the logits predictions from the
+        # inference model.
+        # 构建一个图形，用于计算推理模型中的logits的预测值
+        # 使用CNN，经过卷积和池化层处理。
+        logits = cifar10.inference(images)
 
-    # Calculate loss.
-    #缺失值
-    loss = cifar10.loss(logits, labels)
+        # Calculate loss.
+        # 缺失值
+        loss = cifar10.loss(logits, labels)
 
-    # Build a Graph that trains the model with one batch of examples and
-    # updates the model parameters.
-    #训练建立好的模型
-    train_op = cifar10.train(loss, global_step)
+        # Build a Graph that trains the model with one batch of examples and
+        # updates the model parameters.
+        # 训练建立好的模型
+        train_op = cifar10.train(loss, global_step)
 
-    class _LoggerHook(tf.train.SessionRunHook):
-      """Logs loss and runtime."""
+        class _LoggerHook(tf.train.SessionRunHook):
+            """Logs loss and runtime."""
 
-      def begin(self):
-        self._step = -1
-        self._start_time = time.time()
+            def begin(self):
+                self._step = -1
+                self._start_time = time.time()
 
-      def before_run(self, run_context):
-        self._step += 1
-        return tf.train.SessionRunArgs(loss)  # Asks for loss value.
+            def before_run(self, run_context):
+                self._step += 1
+                return tf.train.SessionRunArgs(loss)  # Asks for loss value.
 
-      def after_run(self, run_context, run_values):
-        if self._step % FLAGS.log_frequency == 0:
-          current_time = time.time()
-          duration = current_time - self._start_time
-          self._start_time = current_time
+            def after_run(self, run_context, run_values):
+                if self._step % FLAGS.log_frequency == 0:
+                    current_time = time.time()
+                    duration = current_time - self._start_time
+                    self._start_time = current_time
 
-          loss_value = run_values.results
-          examples_per_sec = FLAGS.log_frequency * FLAGS.batch_size / duration
-          sec_per_batch = float(duration / FLAGS.log_frequency)
+                    loss_value = run_values.results
+                    examples_per_sec = FLAGS.log_frequency * FLAGS.batch_size / duration
+                    sec_per_batch = float(duration / FLAGS.log_frequency)
 
-          format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
-                        'sec/batch)')
-          print(format_str % (datetime.now(), self._step, loss_value,
-                              examples_per_sec, sec_per_batch))
+                    format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
+                                  'sec/batch)')
+                    print(format_str % (datetime.now(), self._step, loss_value,
+                                        examples_per_sec, sec_per_batch))
 
-    with tf.train.MonitoredTrainingSession(
-        checkpoint_dir=FLAGS.train_dir,
-        hooks=[tf.train.StopAtStepHook(last_step=FLAGS.max_steps),
-               tf.train.NanTensorHook(loss),
-               _LoggerHook()],
-        config=tf.ConfigProto(
-            log_device_placement=FLAGS.log_device_placement)) as mon_sess:
-      while not mon_sess.should_stop():
-        mon_sess.run(train_op)
+        with tf.train.MonitoredTrainingSession(
+            checkpoint_dir=FLAGS.train_dir,
+            hooks=[tf.train.StopAtStepHook(last_step=FLAGS.max_steps),
+                   tf.train.NanTensorHook(loss),
+                   _LoggerHook()],
+            config=tf.ConfigProto(
+                log_device_placement=FLAGS.log_device_placement)) as mon_sess:
+            while not mon_sess.should_stop():
+                mon_sess.run(train_op)
 
 
 def main(argv=None):  # pylint: disable=unused-argument
-#下载和解压缩，函数位于cifar10.py
-#先查找本级目录是否存在数据集文件，若不存在，则在线下载
-  cifar10.maybe_download_and_extract()
+    # 下载和解压缩，函数位于cifar10.py
+    # 先查找本级目录是否存在数据集文件，若不存在，则在线下载
+    cifar10.maybe_download_and_extract()
 
-#判断目录或文件是否存在，如果是，则递归删除train_dir所有目录及其文件
-  if tf.gfile.Exists(FLAGS.train_dir):
-    tf.gfile.DeleteRecursively(FLAGS.train_dir)
-#以递归方式建立train_dir的父目录及其子目录
-  tf.gfile.MakeDirs(FLAGS.train_dir)
-  train()
+# 判断目录或文件是否存在，如果是，则递归删除train_dir所有目录及其文件
+    if tf.gfile.Exists(FLAGS.train_dir):
+        tf.gfile.DeleteRecursively(FLAGS.train_dir)
+# 以递归方式建立train_dir的父目录及其子目录
+    tf.gfile.MakeDirs(FLAGS.train_dir)
+    train()
 
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
